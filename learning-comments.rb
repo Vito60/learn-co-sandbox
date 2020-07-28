ab44df20a1f1e0831eec6410357bc44bc3ccf55d
Randall Williams <randallw1996@gmail.com>
2020-07-28 22:15:08 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,5 @@
+#example of an array 
+the_beatles = ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"]
+
+#example of a hash 
+english_bands_by_city = { :liverpool => "The Beatles", :mancheser => "The Simpsons", :coventry => "Delia Derbyshire and the BBC Radiophonic Band", :london => "Ziggy Stardust and the Spiders from Mars"}
